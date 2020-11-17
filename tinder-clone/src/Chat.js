8c056ab2519d52701aa57ccf7ebb2a668da49008
ajilann <ajilan.nazarudeen@gmail.com>
2020-11-17 16:00:29 +0530

    the chatScreen has been set up

--- tinder-clone/src/Chat.js
+++ tinder-clone/src/Chat.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import "./Chat.css";
 import Avatar from '@material-ui/core/Avatar';
-
+import { Link } from "react-router-dom";
 
 export default function Chat({name, message, profilePic, timestamp }) {
     return (
+        <Link to={`/chat/${name}`}>
         <div className="chat">
             <Avatar className="chat__image" alt={name} 
             src={profilePic} />
@@ -14,5 +15,6 @@ export default function Chat({name, message, profilePic, timestamp }) {
             </div>
             <p className="chat__timestamp">{timestamp}</p>
         </div>
+        </Link>
     )
 }
--- tinder-clone/src/ChatScreen.js
+++ tinder-clone/src/ChatScreen.js
@@ -3,6 +3,7 @@ import "./ChatScreen.css";
 import Avatar from '@material-ui/core/Avatar';
 
 export default function ChatScreen() {
+    const [input, setInput] = useState('');
     const [messages, setMessages] = useState([
         {
             name: "Ellen",
@@ -17,7 +18,14 @@ export default function ChatScreen() {
         {
             message: "Hi, how are you Ellen"
         },
-    ])
+    ]);
+
+    const handleSend = e => {
+        e.preventDefault();
+
+        setMessages([...messages, {message: input }]);
+        setInput("");
+    }
     return (
         <div className="chatScreen">
             <p className="chatScreen__timestamp">YOU MATCHED WITH ELLEN ON 11/10/20</p>
@@ -35,7 +43,18 @@ export default function ChatScreen() {
                    <p className="chatScreen__textUser">{message.message}</p>
                    </div>
                 )
-           ))} 
+           ))}
+
+            <form className="chatScreen__input">
+             <input 
+             value = {input}
+             onChange={e => setInput(e.target.value)}
+             className="chatScreen__inputField"
+             placeholder=" Type a message..."
+             type="text" />
+             <button onClick={handleSend} type="submit" className="chatScreen__inputButton">SEND</button>   
+            </form>   
+    
         </div>
     )
 }
